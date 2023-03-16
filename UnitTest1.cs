e03794370ac60db3908c43ff924d31528db15bc4
riya more <you@example.com>
2023-03-16 14:11:34 +0530

    TestCase1.1

---
+++ UnitTest1.cs
@@ -0,0 +1,16 @@
+namespace MoodAnalyzerTC
+{
+    public class Tests
+    {
+        MoodAnalyserClass mood = new MoodAnalyserClass();
+
+        [Test]
+        public void GivenSadMood_WhenAnalized_ShouldReturnSad()
+        {
+            string result = mood.AnalizeMood("I am in Sad Mood");
+            Assert.AreEqual("Sad", result);
+        }
+
+
+    }
+}
